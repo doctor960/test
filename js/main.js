234d44a51eaf885546e2c3e57f8b8af30a4eb834
doctor960 <sasho12002762@gmail.com>
2025-10-03 13:48:42 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -207,16 +207,19 @@ document.addEventListener("DOMContentLoaded", function() {
     if (hasError) return;
 
     modal.style.display = "block";
+    document.body.style.overflow = "hidden";
     form.reset();
   });
 
   closeBtn.addEventListener("click", function() {
     modal.style.display = "none";
+    document.body.style.overflow = "";
   });
 
   window.addEventListener("click", function(e) {
     if (e.target === modal) {
       modal.style.display = "none";
+      document.body.style.overflow = "";
     }
   });
 });
